dbb0d6301dbc9f86c3e4eccc8446b8761cc4102b
dmitrykachala <kosas@mail.ru>
2021-08-29 07:26:53 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,61 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        return (Win.checkRow(board)) || (Win.checkColumn(board)) || (Win.checkDiagonal(board));
+    }
+
+    public static boolean checkRow(int[][] board) {
+        int success = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    break;
+                }
+                success++;
+                if (success % board.length == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkColumn(int[][] board) {
+        int success = 0;
+        for (int j = 0; j < board.length; j++) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][j] == 0) {
+                    break;
+                }
+                success++;
+                if (success % board.length == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkDiagonal(int[][] board) {
+        int success = 0;
+        for (int i = 0; i < board[i].length; i++) {
+                if (board[i][i] == 0) {
+                    break;
+                }
+                success++;
+                if (success % board.length == 0) {
+                    return true;
+                }
+        }
+        for (int i = 0; i < board.length; i++) {
+                if (board[i][board.length - 1 - i] == 0) {
+                    break;
+                }
+                success++;
+                if (success % board.length == 0) {
+                    return true;
+                }
+        }
+        return false;
     }
 }
